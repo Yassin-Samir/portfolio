42339e2fe92a0eecff90072724e8f2ce42779777
yassin samir <yassinsam3@gmail.com>
2024-09-03 15:44:09 +0300

    removing hover animation from scrollToTop button and maximizing the size of ArrowUp icon

--- app/components/scrollToTop/scrollToTop.tsx
+++ app/components/scrollToTop/scrollToTop.tsx
@@ -12,15 +12,16 @@ function ScrollToTop() {
   }, []);
   return (
     <button
-      className={`fixed bottom-8 right-6 z-[1000] flex items-center rounded-full
-        bg-gradient-to-r from-pink-500 to-violet-600 p-4 hover:text-xl transition-all 
-        duration-300 ease-out ${!ViewScrollButton ? "hidden" : ""}`}
+      className={`fixed bottom-8 right-6 z-[1000] flex justify-center items-center rounded-full
+        bg-gradient-to-r from-pink-500 to-violet-600 p-4 
+        opacity-0 transition-opacity ease-in-out duration-200
+        ${ViewScrollButton ? "!opacity-100" : ""}`}
       disabled={!ViewScrollButton}
       onClick={() => {
         window.scrollTo({ top: 0, behavior: "smooth" });
       }}
     >
-      <FaArrowUp />
+      <FaArrowUp size={20} />
     </button>
   );
 }
