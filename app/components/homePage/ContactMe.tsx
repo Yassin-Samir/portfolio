a036a976d79c21c86c428c3bf0f2d0ce81eb5518
yassin samir <yassinsam3@gmail.com>
2024-09-10 00:34:38 +0300

    changing my contact Email

--- app/components/homePage/ContactMe.tsx
+++ app/components/homePage/ContactMe.tsx
@@ -16,7 +16,7 @@ function ContactMe() {
             size={36}
             className="bg-[#8b98a5] p-2 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
           />
-          <span>yassinex376@gmail.com</span>
+          <span>yassinsam@gmail.com</span>
         </p>
         <p className="text-sm md:text-xl flex items-center gap-3">
           <IoMdCall
